c25528fe76ddab9e1289bd693e6f8a2e7a67ff8f
gaurmohit <mohitgaur0399@gmail.com>
2019-04-10 23:55:17 +0530

    made some small changes

---
+++ GyanSchool/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path,include
+from Auth import views
+from forum import urls
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('secret/', views.secret_page,name='secret'),
+    path('secret2', views.SecretPage.as_view(),name='secret2'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path(r'^oauth/', include('social_django.urls', namespace='social')),
+
+    #For including urls in forum/urls.py
+    path('', include('forum.urls')),
+
+    path('admin/', admin.site.urls),
+]
