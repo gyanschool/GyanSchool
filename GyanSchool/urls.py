f01f897e40dc4477d7ebe6fa30e15b61775b6ba2
RACHIT PATEL <46604432+rachit0905@users.noreply.github.com>
2019-04-15 21:25:53 +0530

    Update urls.py

--- GyanSchool/urls.py
+++ GyanSchool/urls.py
@@ -3,16 +3,17 @@ from django.urls import path,include
 from Auth import views
 from forum import urls
 
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('secret/', views.secret_page,name='secret'),
-    path('secret2', views.SecretPage.as_view(),name='secret2'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path(r'^oauth/', include('social_django.urls', namespace='social')),
 
-    #For including urls in forum/urls.py
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('secret/', views.secret_page, name='secret'),
+    path('secret2', views.SecretPage.as_view(), name='secret2'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('^oauth/', include('social_django.urls', namespace='social')),
+    # For including urls in forum/urls.py
     path('', include('forum.urls')),
 
     path('admin/', admin.site.urls),
+
 ]
